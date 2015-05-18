bbeb344ee21ff5bdccfb91b54a61a8625311ec92
Maxim Dunavicher <maxim.d@velismedia.com>
2015-05-18 14:23:34 +0300

    Starting the project

--- src/sitePackage/CustomizationServlet.java
+++ src/sitePackage/CustomizationServlet.java
@@ -6,14 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @WebServlet(name = "CustomizationServlet", urlPatterns={"/CustomizationServlet"})
 public class CustomizationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         String company_name = request.getParameter("company_name");
-
+        LinkedHashMap<String, String[]> keys_tovalues =  new LinkedHashMap<String, String[]>( request.getParameterMap() ) ;
         Customization cus = new Customization();
+
+        for(Map.Entry<String , String[]>  element : keys_tovalues.entrySet()){
+            System.out.println("KEY:");
+            System.out.println(element.getKey());
+            System.out.println("value");
+            System.out.println(element.getValue()[0]);
+        }
 
         cus.getData();
 
